70644e2d21f72f18f32dca72ef1451a4917d60e7
Alex Petrov <johnsilver7311@gmail.com>
2023-04-26 22:59:52 +0600

    feat: add validation for Dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,6 +12,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status';
 
 @Controller('tasks')
 export class TasksController {
@@ -46,8 +47,8 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus
+    @Body() updateTaskStatus: UpdateTaskStatusDto
   ): Task {
-    return this.tasksService.updateTaskById(id, status);
+    return this.tasksService.updateTaskById(id, updateTaskStatus.status);
   }
 }
